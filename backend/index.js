b749865b9fa35aa854d88a8707cb26bbbadfaa87
Matheus Cardoso <46722962+MatheusCTorres@users.noreply.github.com>
2022-01-14 00:01:07 +0000

    setup

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+require("dotenv").config()
 
 app.use(cors());
 
@@ -35,6 +36,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
+server.listen(process.env.PORT || 3001, () => {
   console.log("server running");
 });
